0a31e9f1a0978eafbef85f1c98861da06ec27e89
oliimendor <endor.sensey@gmail.com>
2024-12-17 20:57:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let a = prompt('String typedagi malumot kiriting');
+let b = NumberDprompt('Number typedagi malumot kiriting');
+let c = Booleanprompt('Boolean typedagi malumot kiriting');
+
+
+alert('Hamma malumotlarni togri kiritganizga ishonchingiz komilmi?');
+
+let a1 = typeof a;
+let b1 = typeof b;
+let c1 = typeof c;
+
+alert(`Siz kiritgan "${a}" sozi ${a1} typega tegishli
+Siz kiritgan "${b}" sozi ${b1} typega tegishli
+Siz kiritgan "${c}" sozi ${c1} typega tegishli`);
